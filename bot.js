44a0968f357e43e6d36558e386088c330429f350
fabioselau077 <57275833+fabioselau077@users.noreply.github.com>
2019-12-09 23:02:56 -0400

    Update

--- bot.js
+++ bot.js
@@ -4,39 +4,32 @@ function pause(milliseconds) {
 }
 
 
-(function abrirPerfil(){
-		var info = document.getElementsByClassName('recCard__info')[0];
-		info.click();
-		pause(1000);
-		tinder();
-}());
 
 function checkTinder() {
 	var base = "https://tinder.com/";
 	return window.location.href.startsWith(base + "app/recs") || window.location.href.startsWith(base + "app/matches");
 }
 
-function tinder(){
-		var nomePessoa = "Paulina";
-		var idadePessoa = 22;
-		console.log(nomePessoa);
-		console.log(idadePessoa);
-			var nome = document.getElementsByClassName('Ov(h) Ws(nw) Ell')[0].textContent;
-			var idade = document.getElementsByClassName('Whs(nw) Fz($l)')[0].textContent;
-			console.log(nome);
-			console.log(idade);
-			pause(1000);
-			if(nome != nomePessoa && idade != idadePessoa){
-				var infoClassName = 'recCard__info';
-				var dislike = document.getElementsByClassName("recsGamepad__button")[0];
-				return dislike.click();
-				openAgainProfile();
-			}else{
-				return console.log("OOPAA, PELO QUE PARECE O PERFIL QUE PROCURA FOI ENCONTRADO!");
+(function tinder(){
+		let bool = true;
+		while (bool){
+				var nomePessoa = "Nome da Pessoa";
+				console.log("Nome da Pessoa que procura: " + nomePessoa);
+				var nome = document.getElementsByClassName('Fxs($flx1) Flw(w) Fz($xl)')[1].textContent;
+				console.log("Nome da Pessoa do Tinder: " + nome);
+				pause(500);
+				if(nome != nomePessoa){
+					var infoClassName = 'recCard__info';
+					var dislike = document.getElementsByClassName("recsGamepad__button")[0];
+					console.log("NÃO É A PESSOA QUE VOCÊ PROCURA!");
+					
+					dislike.click();
+					tinder();
+				}
+				else{
+					bool = false;
+					return console.log("OOPAA, PELO QUE PARECE O PERFIL QUE PROCURA FOI ENCONTRADO!");
+				}
 			}
-}
+}());
 
-function openAgainProfile(){
-	pause(7000);
-	abrirPerfil();
-}
